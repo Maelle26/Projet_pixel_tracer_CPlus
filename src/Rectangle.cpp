91812efa5b43e3155f9d1381a59ece4ba4a92724
lucygrs <lucy.gros@efrei.net>
2026-01-16 11:06:00 +0100

    Rectangle et Square .cpp

---
+++ src/Rectangle.cpp
@@ -0,0 +1,26 @@
+//Fichier Rectangle.cpp
+#include "Rectangle.h"
+#include "Line.h"
+#include "Pixel.h"
+
+
+//Constructeur
+Rectangle::Rectangle(const Point& tl, int w, int h):Shape(),topLeft(tl),width(w),height(h){}
+
+//Constructeur avec coordonnées
+Rectangle::Rectangle(int x, int y, int w, int h):Shape(), topLeft(x,y),width(w),height(h){}
+
+//Méthode pour afficher les informations du Rcetangle
+void Rectangle::print() const{
+    std::cout << "RECTANGLE " << topLeft.getPosX() << " " << topLeft.getPosY()
+              << " " << width << " " << height << std::endl;
+}
+
+//Méthode pour convertir le rectangle en une liste de pixels
+std::vector<Pixel>Rectangle::toPixels() const {
+    // Implémentation de l'algorithme d'Andres/Bresenham
+    // (sera détaiillée dans la partie 2)
+    std::vector<Pixel> pixels;
+    //...code de l'algorithme
+    return pixels
+}
